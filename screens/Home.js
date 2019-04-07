dde0fc1686825833105880ddcd21a84021db78f1
Jon Huber <hubermjonathan@gmail.com>
2019-04-07 04:14:14 -0400

    implemented adding functionality

--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,10 @@ import {
     SafeAreaView,
     View,
     Text,
+    AlertIOS,
 } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Swiper from 'react-native-swiper';
 import { getData, setData } from '../lib/storageFunctions';
 
 const screenWidth = Dimensions.get('window').width;
@@ -17,8 +20,70 @@ export default class HomeScreen extends React.Component {
         super(props);
 
         this.state = {
-            number: 0
+            number: '0'
         }
+
+        this.loader();
+    }
+
+    async loader() {
+        let date = this.currentDate();
+        let data = await getData(date);
+        
+        this.setState({
+            number: data === null ? '0' : data,
+        });
+    }
+
+    currentDate() {
+        let date = new Date();
+        let offsetInHours = date.getTimezoneOffset() / 60;
+        date.setHours(date.getHours() - offsetInHours);
+        let dateShort = date.toJSON().slice(0, 10);
+        return dateShort;
+    }
+
+    undoPress() {
+
+    }
+
+    addPress() {
+        AlertIOS.prompt(
+            'Log Calories',
+            'How many calories do you want to log?',
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {},
+                    style: 'cancel',
+                },
+                {
+                    text: 'Add',
+                    onPress: (number) => {
+                        this.updateData(number);
+                    }
+                },
+            ],
+            'plain-text',
+            '',
+            'numeric'
+        );
+    }
+
+    settingsPress() {
+
+    }
+
+    async updateData(number) {
+        let date = this.currentDate();
+        let newNumber = Math.round(+this.state.number + +number);
+        let data = {
+            date: date,
+            number: newNumber > 10000 ? '10000' : newNumber.toString()
+        }
+
+        await setData(data);
+        this.loader();
     }
 
     render() {
@@ -36,7 +101,9 @@ export default class HomeScreen extends React.Component {
                 </View>
 
                 <View style={styles.barContainer}>
-
+                    <Icon name='undo' type='font-awesome' size={35} onPress={this.undoPress.bind(this)} />
+                    <Icon name='plus' type='font-awesome' size={35} color={highlightColor} reverse onPress={this.addPress.bind(this)} />
+                    <Icon name='cog' type='font-awesome' size={35} onPress={this.settingsPress.bind(this)} />
                 </View>
             </SafeAreaView>
         );
@@ -62,6 +129,9 @@ const styles = StyleSheet.create({
     },
     barContainer: {
         flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
     },
     dropShadow: {
         shadowColor: '#000',
